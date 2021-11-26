16ad35ccf64d46dc9d86f5371ef2a49a0fb5f0e0
gurhankuras <gurhankuras@hotmail.com>
2021-11-26 19:24:32 +0300

    Initial Commit

---
+++ saglik_sigorta/saglik_sigortaApp.swift
@@ -0,0 +1,17 @@
+//
+//  saglik_sigortaApp.swift
+//  saglik_sigorta
+//
+//  Created by Gürhan Kuraş on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct saglik_sigortaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
